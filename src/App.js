2cd8194d0a8c680dc9b096ef49e3dcbd227eba36
klandon2-1736208 <landonkcee@gmail.com>
2020-05-14 18:08:05 -0700

    Add initial firestore setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firestore from './firestore';
+import firebase from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+  example app for populating the firestore database.
+*/
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+     email: '',
+     fullname: ''
+    };
+  }
+
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  addUser = e => {
+    e.preventDefault();
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const userRef = db.collection("users").add({
+      fullname: this.state.fullname,
+      email: this.state.email
+    });
+    this.setState({
+      fullname: '',
+      email: ''
+    });
+  };
+
+  render() {
+    return (
+        <form onSubmit={this.addUser}>
+          <input
+            type="text"
+            name="fullname"
+            placeholder="Full name"
+            onChange={this.updateInput}
+            value={this.state.fullname}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={this.updateInput}
+            value={this.state.email}
+          />
+          <button type="submit">Submit</button>
+        </form>
+    );
+  }
 }
 
 export default App;
